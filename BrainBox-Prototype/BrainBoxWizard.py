30d742a7455f2e5f33514dac56bb473f2169f02c
ahmed-hossam <hossam@brain-bit.com>
2014-07-07 15:39:45 +0200

    Ana estargelt w 3amalt commit 3ala repo
    I added the BrainBox Wizard GUI

--- BrainBox-Prototype/BrainBoxWizard.py
+++ BrainBox-Prototype/BrainBoxWizard.py
@@ -1,7 +1,8 @@
 import ttk
 import Tkinter
-
+global variables
 class Wizard(object, ttk.Notebook):
+    variables = 0
     def __init__(self, master=None, **kw):
         npages = kw.pop('npages', 3)
         kw['style'] = 'Wizard.TNotebook'
@@ -17,11 +18,12 @@ class Wizard(object, ttk.Notebook):
         self._wizard_buttons()
 
     def _wizard_buttons(self):
+#        global variables
         """Place wizard buttons in the pages."""
         for indx, child in self._children.iteritems():
             btnframe = ttk.Frame(child)
             btnframe.pack(side='bottom', fill='x', padx=6, pady=12)
-
+#            nextbtn = ttk.Button(btnframe, text="Next", command=self.next_page(self))            
             nextbtn = ttk.Button(btnframe, text="Next", command=self.next_page)
             nextbtn.pack(side='right', anchor='e', padx=6)
             if indx != 0:
@@ -32,7 +34,10 @@ class Wizard(object, ttk.Notebook):
                 if indx == len(self._children) - 1:
                     nextbtn.configure(text="Finish", command=self.close)
 
-    def next_page(self):
+    def next_page(self):#,variable):
+        print self.current 
+        if self.current == 1:
+            print "Page 1" , self.variables
         self.current += 1
 
     def prev_page(self):
@@ -58,6 +63,7 @@ class Wizard(object, ttk.Notebook):
         desc=ttk.Label(body, text='Welcome to the BrainBox Wizard. Please follow the wizard in order to register\nyour device and callibrate the BCI headset with your appplications.\nPlease connect the headset into the USB port.').pack(pady = 20)
 
     def secondpage(self, body):
+        variables = []
         desc=ttk.Label(body, text='Please fill in the registration form below to set up your account with the\nBrainPrint service, and to setup BrainBox.\nPlease make sure you have a proper Internet Connection!').pack(pady = 20)
         seperator = ttk.Frame(height=2)
         seperator.pack()
@@ -65,21 +71,34 @@ class Wizard(object, ttk.Notebook):
         name=ttk.Label(body, text='Name').pack(pady=0)
         nameentry= ttk.Entry(body)
         nameentry.pack()
+        #nameentry.insert(0,'Enter your name :')
+        variables.append(nameentry)
+        
         email=ttk.Label(body, text='Email').pack(pady=10)
         emailentry= ttk.Entry(body)
         emailentry.pack()
+        variables.append(emailentry)
+        
         loation=ttk.Label(body, text='Location').pack(pady=10)
         locationentry= ttk.Entry(body)
         locationentry.pack()
+        variables.append(locationentry)
+
         user=ttk.Label(body, text='User').pack(pady=5)
         userentry= ttk.Entry(body)
         userentry.pack()
+        variables.append(userentry)
+        
         password=ttk.Label(body, text='Password').pack(pady=10)
         passentry= ttk.Entry(body)
         passentry.pack()
+        variables.append(passentry)
+        
         device=ttk.Label(body, text='Device').pack(pady=10)
         deviceentry= ttk.Entry(body)
         deviceentry.pack()
+        variables.append(deviceentry)
+        return variables
         
     def thirdpage(self, body):
         body.pack(side='top', fill='both', padx=6, pady=12)
@@ -109,6 +128,7 @@ class Wizard(object, ttk.Notebook):
 
 
 def demo():
+    variables = 0
     root = Tkinter.Tk()
     wizard = Wizard(npages=3)
     wizard.master.minsize(400, 350)
